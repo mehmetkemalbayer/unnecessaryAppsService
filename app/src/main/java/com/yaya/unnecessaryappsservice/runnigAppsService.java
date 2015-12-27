aebffa2d764a33f9aa825507a338772f78bbb8fb
mehmetkemalbayer <mkb@MKB-MacBook-Air.local>
2015-12-28 00:05:54 +0200

    adds main activity

--- app/src/main/java/com/yaya/unnecessaryappsservice/runnigAppsService.java
+++ app/src/main/java/com/yaya/unnecessaryappsservice/runnigAppsService.java
@@ -3,7 +3,9 @@ package com.yaya.unnecessaryappsservice;
 import android.app.ActivityManager;
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -11,9 +13,25 @@ import java.util.List;
  */
 public class runnigAppsService extends IntentService{
 
+    public runnigAppsService() {
+        super("runningAppsService");
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
+        android.os.Debug.waitForDebugger();
         ActivityManager activityManager = (ActivityManager)getSystemService(ACTIVITY_SERVICE);
         List<ActivityManager.RunningAppProcessInfo> runningAppProcessInfoList = activityManager.getRunningAppProcesses();
+        StringBuilder strBuilder = null;
+        for(Iterator<ActivityManager.RunningAppProcessInfo> i = runningAppProcessInfoList.iterator(); i.hasNext(); ) {
+            ActivityManager.RunningAppProcessInfo item = i.next();
+            strBuilder = new StringBuilder();
+            for (int j = 0; j < item.pkgList.length; j++) {
+                strBuilder.append( item.pkgList[j] );
+            }
+            String newString = strBuilder.toString();
+
+            System.out.println("Kemal" + strBuilder.toString());
+        }
     }
 }
